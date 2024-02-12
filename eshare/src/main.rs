8acc13823cfc5f604507c75a89b1933c4934fbe6
fks <f4ferris@gmail.com>
2024-02-12 11:22:23 -0500

    inital commit

---
+++ eshare/src/main.rs
@@ -0,0 +1,7 @@
+extern crate ft;
+
+fn main() {
+    let flag = std::env::args().nth(1).expect("no flag given");
+    let file = std::env::args().nth(2).expect("no file given");    
+    println!("flag: {:?}, path: {:?}", pattern, path)
+}
